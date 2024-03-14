5ce35b48514b1fb4621c99315a504e63a1f1220b
Tuğrul Berk Şekli <berksekli@gmail.com>
2024-03-14 14:12:54 +0300

    scriptler ve css düzenlendi

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const form = document.getElementById('todo-form');
 const input = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
+const doneList = document.getElementById('done-list');
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -15,6 +16,17 @@ function addTodoItem(todoText) {
     const li = document.createElement('li');
     li.textContent = todoText;
     todoList.appendChild(li);
+    li.addEventListener('click', function() {
+        li.remove();
+        moveTodoToDone(li.textContent);
+    });
+}
+
+function moveTodoToDone(todoText) {
+    const li = document.createElement('li');
+    li.texrContent = todoText;
+    doneList.appendChild(li);
+
     li.addEventListener('click', function() {
         li.remove();
     });
